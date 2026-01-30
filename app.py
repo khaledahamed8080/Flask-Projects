075a322bb4d2bcf30e144f2d221524ed81af4f20
khaled <khaledahamed8080@gmail.com>
2026-01-30 13:40:29 +0530

    Third mini project

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template , request, redirect, url_for, session
+from flask import Flask, render_template , request, redirect, url_for, session, flash
 
 
 app=Flask(__name__)
+app.secret_key="my_secret_key"
 
-
-@app.route("/feedback",methods=["GET","POST"])
-def feedback():
+@app.route("/",methods=["GET","POST"])
+def form():
      if request.method=="POST":
-          name=request.form.get("username")
-          message=request.form.get("message")
+          name=request.form.get("name")
+          if not name:
+               flash("Name cannot be empty")
+               return redirect(url_for("form"))
+          flash(f"Thank you {name}, your feedback is valuable to us")          
+          return redirect(url_for("thankyou"))
+     return render_template("form.html")
 
-          return render_template("thankyou.html",user=name,message=message)
-     return render_template("feedback.html")
+
+@app.route("/thankyou")
+def thankyou():
+    return render_template("thankyou.html")
 
 
